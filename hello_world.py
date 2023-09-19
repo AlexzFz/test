76702c8d6a8ef85c828fa194bd4a875916657dd5
AlexxFz <superwolf03@mail.ru>
2023-09-19 19:57:20 +0700

    Вывод 'Hello, world\!' в консольи и в файл

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,8 @@
-print("Hello, world!")
+message = "Hello, world!"
+
+# Вывести сообщение в консоль
+print(message)
+
+# Записать сообщение в файл
+with open("output.txt", "w") as file:
+    file.write(message)
